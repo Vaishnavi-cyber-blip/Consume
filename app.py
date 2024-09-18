918ae67e8a3084346a8baecffcb75b1e7338172c
Vaishnavi-cyber-blip <72164140+Vaishnavi-cyber-blip@users.noreply.github.com>
2024-09-19 01:43:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import re
 
 app = Flask(__name__)
 # CORS(app3, resources={r"/*": {"origins": "http://localhost:5173"}})  # Allow only your frontend
-CORS(app, resources={r"/*": {"origins": "https://friendly-spork-vecd.onrender.com"}})
+CORS(app, resources={r"/*": {"origins": "https://friendly-spork-2.onrender.com"}})
 
 groq_api_key = os.getenv("GROQ_API_KEY")
 client = Groq(api_key=groq_api_key)
